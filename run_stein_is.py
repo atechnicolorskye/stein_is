d00f2c89dacbdeab8721d5f57ad3d59095cd9b50
Sky <a_technicolor_skye@hotmail.com>
2018-02-23 15:42:46 +0900

    Have process before session, testing memory usage

--- run_stein_is.py
+++ run_stein_is.py
@@ -12,39 +12,32 @@ import pdb
 # Check kernel outputs make sense: They do, but Fisher seems to be less discriminative than SE in adversarial conditions
 # Check if Stein conidtions hold: Possibly, much smaller KSD when drawn from actual distribution
 
-def stein_is_session(params):
-    with tf.Session(config=tf.ConfigProto(
-        device_count={'CPU':1, 'GPU':0},
-        allow_soft_placement=True,
-        log_device_placement=False,
-        inter_op_parallelism_threads=1,
-        intra_op_parallelism_threads=1
-    )
-    )as sess:
-        [kernel, target, n_runs, initial_mu, initial_sigma, n_leaders, n_followers, iterations, step_size_alpha, step_size_beta, sess_num] = params
+def stein_is_process(params):
+    [kernel, target, n_runs, initial_mu, initial_sigma, n_leaders, n_followers, iterations, step_size_alpha, step_size_beta, proc_num] = params
+    print 'Process ' + str(proc_num) + ' has started'
+    proc_start = time.time()
 
-        print 'Session ' + str(sess_num) + ' has started'
+    proc_MSE = []
+    proc_MSE_scaled = []
+    proc_NC = []
+    proc_start = time.time()
+    c_pool = 0
+    # log_q_update = np.zeros(n_followers)
 
-        sess_MSE = []
-        sess_MSE_scaled = []
-        sess_NC = []
-        sess_start = time.time()
-        c_pool = 0
-        # log_q_update = np.zeros(n_followers)
+    # Initialise GMM
+    # Simplest
+    # mu = np.array([1.]); sigma = np.sqrt(np.array([2.0])); weights = np.array([1.]); dim = 1
+    if target == 'stein_is':
+        # Stein IS
+        mu = np.array([[-.5], [.5], [-1.], [1.0], [-1.5], [1.5], [-2.0], [2.0], [-2.5], [2.5]]); sigma = np.sqrt(2) * np.ones(10); weights = (1 / 10.0) * np.ones(10); dim = 2
+    elif target == 'ais':
+        # Neal
+        mu = np.array([1., -1.]); sigma = np.array([0.1, 0.05]); weights = np.array([1. / 3, 2. / 3]); dim = 6
+    elif target == '6d_gauss':
+        # 6D Gaussian for sanity check
+        mu = np.array([1.]); sigma = np.array([0.1]); weights = np.array([1.]); dim = 6
 
-        # Initialise GMM
-        # Simplest
-        # mu = np.array([1.]); sigma = np.sqrt(np.array([2.0])); weights = np.array([1.]); dim = 1
-        if target == 'stein_is':
-            # Stein IS
-            mu = np.array([[-.5], [.5], [-1.], [1.0], [-1.5], [1.5], [-2.0], [2.0], [-2.5], [2.5]]); sigma = np.sqrt(2) * np.ones(10); weights = (1 / 10.0) * np.ones(10); dim = 2
-        elif target == 'ais':
-            # Neal
-            mu = np.array([1., -1.]); sigma = np.array([0.1, 0.05]); weights = np.array([1. / 3, 2. / 3]); dim = 6
-        elif target == '6d_gauss':
-            # 6D Gaussian for sanity check
-            mu = np.array([1.]); sigma = np.array([0.1]); weights = np.array([1.]); dim = 6
-
+    with tf.Graph().as_default():
         # Initialise mixture model
         mm = GMM(mu, sigma, weights, dim)
         # model = SteinIS(mm, dim, n_leaders, n_followers, initial_mu, initial_sigma, kernel)
@@ -52,43 +45,51 @@ def stein_is_session(params):
 
         for run in range(n_runs):
             # Set seed
-            tf.set_random_seed((sess_num * n_runs) + run)
-            # Intialise graph after setting random seed to make deterministic
-            model = SteinIS(mm, dim, n_leaders, n_followers, initial_mu, initial_sigma, kernel)
-            sess.run(tf.global_variables_initializer())
-            run_start = time.time()
-            # B, q_density, A = sess.run(initialise_variables(initial_mu, initial_sigma, n_leaders, n_followers, dim))
-            # print sess_num, run, sess.run(model.A)
-            # pdb.set_trace()
-            for i in range(1, iterations + 1):
-                step_size = step_size_alpha * (1. + i) ** (-step_size_beta)
-                # Think about dx_log_pA and what it represents, why does it dominate the update term?
-                # Can we do something like Adagrad where we maximise log_pA in a 'unit ball' regardless of points?
-                _ = sess.run([model.updates], feed_dict={model.step_size: step_size})
-                # if i == 1:
-                #     pass
-                # else:
-                #     print log_B - log_B_old
-                # log_B_old = log_B
-                # _, A, log_pA, dx_log_pA = sess.run([model.updates, model.A, model.log_pA, model.dx_log_pA], feed_dict={model.step_size: step_size})
-                # A, A_dw_log_px, A_dmu_log_px, A_dsigma2_log_px, A_dmu_log_px_ = sess.run([model.updates, model.A, model.A_dw_log_px, model.A_dmu_log_px, model.A_dsigma2_log_px, model.A_dmu_log_px_], feed_dict={model.step_size: step_size})
+            with tf.Session(config=tf.ConfigProto(
+                    device_count={'CPU':1, 'GPU':0},
+                    allow_soft_placement=True,
+                    log_device_placement=False,
+                    inter_op_parallelism_threads=1,
+                    intra_op_parallelism_threads=1
+                    )
+                )as sess:
+                tf.set_random_seed((proc_num * n_runs) + run)
+                # Intialise model after setting random seed to make deterministic
+                model = SteinIS(mm, dim, n_leaders, n_followers, initial_mu, initial_sigma, kernel)
+                sess.run(tf.global_variables_initializer())
+                run_start = time.time()
+                # B, q_density, A = sess.run(initialise_variables(initial_mu, initial_sigma, n_leaders, n_followers, dim))
+                # print proc_num, run, '\n', sess.run(model.A)
                 # pdb.set_trace()
-                if i % 800 == 0:
-                    normalisation_constant = np.sum(sess.run(tf.exp(model.m_model.log_px(model.B)) / (model.q_density * tf.exp(-model.log_q_update)))) / n_followers
-                    sess_NC.append(normalisation_constant)
-                    if target == 'stein_is':
-                        sess_MSE.append((normalisation_constant - 1) ** 2)
-                    elif target == 'ais':
-                        sess_MSE_scaled.append((np.abs(normalisation_constant - 0.000744) / 0.000744) ** 2)
-                        sess_MSE.append((normalisation_constant - 0.000744) ** 2)
-                    elif target == '6d_gauss':
-                        sess_MSE_scaled.append((np.abs(normalisation_constant - 0.000248) / 0.000248) ** 2)
-                        sess_MSE.append((normalisation_constant - 0.000248) ** 2)
-            run_time = time.time() - run_start
-            print 'Run ' + str(run) + ' took ' + str(run_time) + ' seconds'
-    print 'Session complete'
-    print str(n_runs) + ' runs took ' + str(time.time() - sess_start) + ' seconds'
-    return sess_MSE, sess_MSE_scaled, sess_NC
+                for i in range(1, iterations + 1):
+                    step_size = step_size_alpha * (1. + i) ** (-step_size_beta)
+                    # Think about dx_log_pA and what it represents, why does it dominate the update term?
+                    # Can we do something like Adagrad where we maximise log_pA in a 'unit ball' regardless of points?
+                    _ = sess.run([model.updates], feed_dict={model.step_size: step_size})
+                    # if i == 1:
+                    #     pass
+                    # else:
+                    #     print log_B - log_B_old
+                    # log_B_old = log_B
+                    # _, A, log_pA, dx_log_pA = sess.run([model.updates, model.A, model.log_pA, model.dx_log_pA], feed_dict={model.step_size: step_size})
+                    # A, A_dw_log_px, A_dmu_log_px, A_dsigma2_log_px, A_dmu_log_px_ = sess.run([model.updates, model.A, model.A_dw_log_px, model.A_dmu_log_px, model.A_dsigma2_log_px, model.A_dmu_log_px_], feed_dict={model.step_size: step_size})
+                    # pdb.set_trace()
+                    if i % iterations == 0:
+                        normalisation_constant = np.sum(sess.run(tf.exp(model.m_model.log_px(model.B)) / (model.q_density * tf.exp(-model.log_q_update)))) / n_followers
+                        proc_NC.append(normalisation_constant)
+                        if target == 'stein_is':
+                            proc_MSE.append((normalisation_constant - 1) ** 2)
+                        elif target == 'ais':
+                            proc_MSE_scaled.append((np.abs(normalisation_constant - 0.000744) / 0.000744) ** 2)
+                            proc_MSE.append((normalisation_constant - 0.000744) ** 2)
+                        elif target == '6d_gauss':
+                            proc_MSE_scaled.append((np.abs(normalisation_constant - 0.000248) / 0.000248) ** 2)
+                            proc_MSE.append((normalisation_constant - 0.000248) ** 2)
+                run_time = time.time() - run_start
+                print 'Run ' + str(run) + ' took ' + str(run_time) + ' seconds'
+    print 'Process complete'
+    print str(n_runs) + ' runs took ' + str(time.time() - proc_start) + ' seconds'
+    return proc_MSE, proc_MSE_scaled, proc_NC
 
 
 if __name__ == '__main__':
@@ -102,8 +103,8 @@ if __name__ == '__main__':
     # Hyperparameters
     p['kernel'] = 'se'
     p['target'] = 'stein_is'
-    p['n_processes'] = 10
-    p['n_runs'] = 5
+    p['n_processes'] = 20
+    p['n_runs'] = 25
     p['iterations'] = 1200
     p['step_size_alpha'] = np.float64(.00005)
     p['step_size_beta'] = np.float64(0.5)
@@ -116,7 +117,7 @@ if __name__ == '__main__':
     pool = Pool(processes=p['n_processes'])
     for i in range(p['n_processes']):
         # with tf.device('/gpu:%d' % i):
-        results.append(pool.apply_async(stein_is_session, [params[i]]))
+        results.append(pool.apply_async(stein_is_process, [params[i]]))
     pool.close()
     # # Alternative
     # # results = pool.map_async(stein_is_session, params)
@@ -139,7 +140,7 @@ if __name__ == '__main__':
     output['NC'] = NC
     output['params'] = p
     np.save(str(p['kernel']) + '_' + str(p['target']) + '_a_' + str(p['step_size_alpha']) + '_b_' + str(p['step_size_beta']) + '_l_' + str(p['n_leaders']) + '_f_' + str(p['n_followers']) + '_sig2_' + str(p['initial_sigma'] ** 2) + '_runs_' + str(p['n_runs'] * p['n_processes']) + '.npy', output)
-    # print 'Done'
+    print 'Done! Total Time: ' + str(time.time() - start_time) + ' seconds'
     print np.mean(MSE), np.mean(MSE_scaled), np.mean(NC)
 
 
